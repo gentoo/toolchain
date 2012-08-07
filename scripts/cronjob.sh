bb354c69e2fd8d10f7721c946fa78b7a3dc23e89
Mike Frysinger <vapier@gentoo.org>
2012-08-07 20:02:42 +0000

    make a bit more robust, and fix working with newer svn versions

--- scripts/cronjob.sh
+++ scripts/cronjob.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+
+# Make sure we only have one instance running at a time.
+[[ ${FLOCKER} != $0 ]] && exec env FLOCKER=$0 flock -en $0 -c "$0 $*" || :
+
 set -e
 cd "${0%/*}"/..
 (
+# XXX: Maybe add broken lock/cleanup detection?
 svn up -q
 
 doit() {
 	./scripts/update-$1
 	cd sys-devel/$1
-	svn add -q * || :
+
+	# Just in case someone else made a commit before we did.
+	svn up -q
+
+	# Need the force as newer svn versions (1.7) don't like being
+	# given files that already exist.
+	svn add -q --force * || :
+
 	[[ -z $(svn st | grep -v '[^AM]') ]]
 	svn commit -m "update $1 snapshots"
+
 	cd ../..
 }
 
