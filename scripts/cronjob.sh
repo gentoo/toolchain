cc84c22710e8414ec7e3a3423292784fba599d6d
Mike Frysinger <vapier@gentoo.org>
2014-06-14 19:50:54 -0400

    migrate cronjob to use git instead

--- scripts/cronjob.sh
+++ scripts/cronjob.sh
@@ -7,45 +7,34 @@ NCPUS=$(getconf _NPROCESSORS_ONLN)
 set -e
 cd "${0%/*}"/..
 
-svn_commit() {
-	# Just in case someone else made a commit before we did.
-	svn up -q
-
-	# Need the force as newer svn versions (1.7) don't like being
-	# given files that already exist.
-	svn add -q --force * || :
-
-	local st=$(svn st .)
-
-	local d=$(echo "${st}" | awk '$1 ~ /^[!?]/ { print $NF }')
-	if [[ -n ${d} ]] ; then
-		svn rm ${d}
-	fi
-
-	if [[ -z $(svn st . | grep -v '[^AM]') ]] ; then
-		svn commit -m "$1" .
-	fi
+commit() {
+	git add .
+	git commit -a -m "$1" .
 }
 
 doit() {
 	./scripts/update-$1
 	cd sys-devel/$1
-	svn_commit "update $1 snapshots"
+	commit "update $1 snapshots"
 	cd ../..
 }
 
 main() {
 	(
-	# XXX: Maybe add broken lock/cleanup detection?
-	svn upgrade || :
-	svn revert -R .
-	svn up -q
+	git reset --hard origin/master
+	git checkout -f
+	git pull
 	doit gcc
 	doit gdb
 
 	egencache --repo=toolchain --update --portdir-overlay="${PWD}" -j ${NCPUS:-1}
 	cd metadata
-	svn_commit "update metadata"
+	commit "update metadata"
+
+	# Just in case someone else made a commit before we did.
+	git pull --rebase
+
+	git push
 	) >& scripts/cronjob.log
 }
 main "$@"
